97d379deb80329367442ca4b5be7994e8af06440
khati <khati@DESKTOP-B2D93F2>
2023-01-25 21:26:13 -0600

    I am uploading all files of my project for the first time

---
+++ src/Training/Hello.java
@@ -0,0 +1,17 @@
+package Training;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to the world of DevOps");
+		System.out.println(20+40);
+		
+		
+		
+		
+		
+	}
+
+}
